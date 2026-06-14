68fb90b1fb70da2fa7a347ea2a7a8e39e8a2dfe2
10345tarushsingla10c-tech <10345tarushsingla10c@gmail.com>
2026-06-14 12:29:42 +0000

    changes config done

---
+++ 1_basic/1_variables.js
@@ -0,0 +1,15 @@
+const Password = 123456789
+let accountEmail  = "Tarush@gmail.com"
+let accountId = "2321"
+let accountCity = "Ludhiana"
+let accountState = "Punjab"
+
+
+//Password = 21568//  It is not allowed we cannot change constant
+console.log("Passowrd")
+
+accountEmail = "singla@g.c"
+accountCity = "patiala"
+accountId = "0101"
+accountState = "Haryana"
+console.table([Password , accountEmail , accountId , accountCity , accountState])
