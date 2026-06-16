c7d74cf9f8ff5342a255637c6fe1e0424be5176f
10345tarushsingla10c-tech <10345tarushsingla10c@gmail.com>
2026-06-16 04:59:05 +0000

    conversion

--- 1_basic/conversion.js
+++ 1_basic/conversion.js
@@ -1,5 +1,21 @@
-let IsThis = (1)
+//let IsThis = (1)
 
-let booleanIsThis = Boolean(IsThis)
-console.log (booleanIsThis)
+//let booleanIsThis = Boolean(IsThis)
+//console.log (booleanIsThis)
+
+//let IsThis = (0)
+
+//let booleanIsThis = Boolean(IsThis)
+//console.log (booleanIsThis)
+
+let IsNumber = 23
+// let StringNumber = String(IsNumber)
+// console.log(StringNumber)
+// console.log(typeof StringNumber)
+
+// ****************** Operations *******************
+
+let value = 16
+let negval = -value
+console.log(negval)
 
