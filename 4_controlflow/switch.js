877473ca0221a08d7c170465f092e9168e6d2877
10345tarushsingla10c-tech <10345tarushsingla10c@gmail.com>
2026-07-14 21:53:10 +0000

    switch

---
+++ 4_controlflow/switch.js
@@ -0,0 +1,23 @@
+const month = 4
+switch (month) {
+    case 1:
+        console.log("januray");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+// when we write switch it comes bluepritn come automatically
+    default:
+        console.log("write what you want to get executed");
+        // default case is that when no case is matched then it will get printed
+           break;
+}
