5dc743f20673d6332cf0ea65273cede9efbe2503
10345tarushsingla10c-tech <10345tarushsingla10c@gmail.com>
2026-07-14 21:53:30 +0000

    truthy and falsy

---
+++ 4_controlflow/truthy.js
@@ -0,0 +1,20 @@
+const name = "Tarush"
+if (name){
+    console.log("my name is Tarush"); 
+}else {    console.log("no identity");}
+
+
+const age = ""
+if (age){
+    console.log("my name is Tarush"); 
+}else {    console.log("no identity");}
+// so this is a falsy value as it is an empty string 
+
+// false value means which dosent give any answer
+
+//  falsy value are 0 , -0 , false , BigInt 0n , "" , undefined , null
+
+// truthy values are " " , "0" , true , [] , () , {}
+
+
+    
